494701cf2f703282c8460145db98dd4bf6fcab73
bison2 <profvaldes@gmail.com>
2021-03-11 22:44:47 -0300

    ta rodando local no heroku da bad request 400

--- DBR_project/settings/base.py
+++ DBR_project/settings/base.py
@@ -28,8 +28,8 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=config('DEBUG', default = False, cast = bool) 
 
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) 
-ALLOWED_HOSTS =127.0.0.1, .localhost, https://ribsontcc2021.herokuapp.com/
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) 
+
 
 # Application definition
 
