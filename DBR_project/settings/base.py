230ff615308771adb65571789ad1a8a720f792b0
bison2 <profvaldes@gmail.com>
2021-03-17 09:26:21 -0300

    anulando o decouple

--- DBR_project/settings/base.py
+++ DBR_project/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config, Csv
+#from decouple import config, Csv
 import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+#SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY='gbs5*v$wa=dz@a^u=1+kpht^f#nh61o9z3l@5-@2v0_4$@cqs4'
 
+
+ALLOWED_HOSTS= ['127.0.0.1' 'localhost' 'git.heroku.com/ribson.git']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=config('DEBUG', default = False, cast = bool) 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) 
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) 
 
 
 # Application definition
