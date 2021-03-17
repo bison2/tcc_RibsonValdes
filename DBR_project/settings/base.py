bfbc46e1f67e3f8aad13716c0c0088d29d6c70af
bison2 <profvaldes@gmail.com>
2021-03-17 11:55:33 -0300

    mudancas nas imagens da home e do footer

--- DBR_project/settings/base.py
+++ DBR_project/settings/base.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY=config('SECRET_KEY')
 
+
 SECRET_KEY=os.getenv('SECRET_KEY') 
 
 ALLOWED_HOSTS= ['127.0.0.1' 'localhost' 'git.heroku.com/ribson.git']
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG=config('DEBUG', default = False, cast = bool) 
-DEBUG = False
+DEBUG = False #retornar para False no deploy
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) 
 
 
